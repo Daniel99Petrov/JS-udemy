d1a533b4051d27c67e711001c0eb209a341264ef
Daniel Petrov <daniplpetrov@gmail.com>
2023-11-23 14:27:16 +0200

    feat: add module 1 done

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,47 @@
+/*
+const country = 'Bulgaria';
+const continent = 'Europe';
+let population = 6;
+
+console.log(country,continent,population);
+
+const isIsland = false;
+const language = 'Bulgarian';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+const splittedPopulation = population / 2;
+console.log(splittedPopulation);
+
+console.log(++population);
+
+let populationFinland = 6;
+console.log(population > populationFinland);
+
+let avgPopulation = 33;
+console.log(population > avgPopulation);
+
+const description = `${country} is in ${continent} and its' ${population} million people speak ${language}`;
+
+console.log(description);
+
+if(population < 33) {
+    console.log(`${country}'s population is ${33-population} million below average`);
+} else {
+    console.log(`${country}'s population is above average`);
+}
+
+const numNeighbours = Number(prompt(`How many neighbour countries does your country have?`));
+
+if(numNeighbours === 1){
+    console.log(`Only 1 border`);
+} else if (numNeighbours > 1) {
+    console.log(`More than 1 border`);
+} else {
+    console.log(`No borders`);
+}
+*/
+
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,109 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+true;
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+
+
+
+var job = 'programmer';
+job = 'teacher';
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas*2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+const age = 14;
+
+const isOldEnough = age >=18;
+
+if(isOldEnough) {
+    console.log(`Ready`)
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`${yearsLeft} years left`);
+}
+
+
+const favourite = Number(prompt("Your number"));
+console.log(favourite);
+
+if(favourite === 23){
+    console.log(`cool`);
+} else if(favourite === 7){
+    console.log(`Also cool`);
+} else {
+    console.log(`Sad`);
+}
+
+const day = `Monday`;
+
+switch (day) {
+  case `Monday`:
+    console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+    break;
+  case `Tuesday`:
+    console.log(`Prepare theory videos`);
+    break;
+  case `Wednesday`:
+  case `Thursday`:
+    console.log(`Write code examples`);
+    break;
+  case `Friday`:
+    console.log(`Eeeeasy`);
+    break;
+  default:
+    console.log(`Not a weekday`);
+}
+*/
+
+const age = 24;
+
+const drink = age >= 18 ? `Rakia` : `Water`;
+console.log(drink);
+
+let drink2;
+if(age>=18){
+    drink2 = `wine`;
+} else {
+    
+}
